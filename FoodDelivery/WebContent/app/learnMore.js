001d59245bda3c393f1f0c3c2d91970c1ad4fb81
Branimirius <73712742+Branimirius@users.noreply.github.com>
2021-09-06 17:56:25 +0200

    frontend fix

--- FoodDelivery/WebContent/app/learnMore.js
+++ FoodDelivery/WebContent/app/learnMore.js
@@ -2,52 +2,70 @@ function filterArticles(articles, id) {
 	
 	return articles.filter(el => el.restaurant == id);
 }
+
+function findRestaurant(restaurants, id){
+	return restaurants.find(el => el.id == id);
+}
 Vue.component("learn-more", {
 	props:['id'],
 	data: function () {
 		    return {
 		      articles: null,
-		      filteredArticles: null,
-		      total: 0,
+		      restaurant: { },
 		      restaurantId: this.$route.params.id
 		    }
 	},
 		
 	template: ` 
-<div>
-		Raspolozivi artikli {{ restaurantId }}:
-		<table border="1">
-		<tr bgcolor="lightgrey">
-			<th>Naziv</th><th>Opis</th><th>Cena</th><th>&nbsp;</th>
-			<tr bgcolor="lightgrey" v-for="a in articles" >
-				<td> {{a.name}}</td>
-				<td> {{a.description}}</td>
-				<td> {{a.price}} </td>
-				<td>
-					
-					<button v-on:click="addToCart(p)">Dodaj</button>
-				</td>			
-			</tr>
-		</table>
-		<br /> 
-		
-	
-	
-</div>		  
+
+<tbody class = "articles">
+	<div class="container emp-profile" v-if="restaurant">
+		<div class="row">
+		    <div class="col-md-4">
+		        <div class="profile-img">
+		            <img v-bind:src="restaurant.logoPath" alt=""/>
+		        </div>
+		    </div>
+		    <div class="col-md-6">
+		        <div class="profile-head">
+		                    <h5>
+		                        {{ restaurant.name }}
+		                    </h5>
+		                    <h6>
+		                        {{ restaurant.title }}
+		                    </h6>
+		                    <p class="proile-rating">TYPE : <span> {{ restaurant.type }} </span></p>
+		        </div>
+		    </div>
+		</div>
+	</div>
+	<div class = "orange-title">Articles in restaurant: </div>
+	<ul class = "articles" v-for="a in articles">
+	   <li class = "articles">
+	      <img class = "articles" v-bind:src="a.picturePath" style="width: 5rem; height: 5rem; opacity: 1; float: right;" />
+	      <h3> {{ a.name }} </h3>
+	      <p> {{ a.description }} </p>
+	      {{ a.price }}din
+	      <button v-on:click="addToCart(a)" style="float: right;" >Add to cart</button>
+	    
+	    </li>
+	</ul>
+</tbody>	  
 `
 	, 
 	methods : {
-//		addToCart : function (product) {
-//			axios
-//			.post('rest/proizvodi/add', {"id":''+product.id, "count":parseInt(product.count)})
-//			.then(response => (toast('Product ' + product.name + " added to the Shopping Cart")))
-//		}
+		addToCart : function(article) {
+			alert("You have to log in first.");
+			window.location.href = "http://localhost:3665/FoodDelivery/login.html";
+		}
 	},
 	mounted () {
         axios
           .get('rest/articles/getArticles')
           .then(response => (this.articles = filterArticles(response.data, this.restaurantId)));
-        console.log(this.id);
-        //this.filteredArticles = response.data.filter(el => el.restaurant == this.id)
-    }
+        axios
+        .get('rest/restaurants/getRestaurants')
+        .then(response => (this.restaurant = findRestaurant(response.data, this.restaurantId)));
+  
+	}
 });
